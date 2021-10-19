5f8c4d719b7088d886de2a07b55e2b2925741d3f
landinodc <landinodc@gmail.com>
2021-10-18 23:30:12 -0400

    Initial Commit, words object implemented

---
+++ MixedMessages/main.js
@@ -0,0 +1,18 @@
+/*
+    
+    Project: Mixed Messages
+    Filename: main.js
+    Description: core file for mixed messages project, returns a random message.
+    Author: Luis Andino - landinodc@lendingpoint.com
+
+*/
+
+// Words object
+const words = {
+    adjectives = ['perfect', 'amazing', 'lovely', 'big', 'blue'],
+    nouns = ['house', 'dog', 'town', 'computer', 'lamp'],
+    activeverb = ['jump', 'laugh', 'spin'],
+    conjunction = ['because', 'after', 'while'],
+    adverb = ['usually', 'normally', 'typically'],
+    pronoun = ['She', 'He', 'They']
+}
