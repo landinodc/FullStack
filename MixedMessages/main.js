5966562327eb87773e803fd1bfb281c341035b06
landinodc <landinodc@gmail.com>
2021-10-18 23:45:16 -0400

    Fixed word object bug

--- MixedMessages/main.js
+++ MixedMessages/main.js
@@ -9,21 +9,23 @@
 
 // Words object
 const words = {
-    adjectives = ['perfect', 'amazing', 'lovely'],
-    nouns = ['house', 'dog', 'town'],
-    activeverb = ['jump', 'laugh', 'spin'],
-    conjunction = ['before', 'after', 'when'],
-    adverb = ['usually', 'normally', 'typically'],
-    pronoun = ['She', 'He', 'They'],
-    otherverb = ['seeing', 'smelling', 'observing']
+    adjectives: ['perfect', 'amazing', 'lovely'],
+    nouns: ['house', 'dog', 'town'],
+    activeverb: ['jump', 'laugh', 'spin'],
+    conjunction: ['before', 'after', 'when'],
+    adverb: ['usually', 'normally', 'typically'],
+    pronoun: ['She', 'He', 'They'],
+    otherverb: ['seeing', 'smelling', 'observing']
 }
 
+// Returns randum int between 0-2
 function randNum3() {
     return Math.floor(Math.random() * 3)
 }
 
+// generates and returns message
 function returnMessage () {
-    // adding some logiv to add 's'
+    // adding some logic to add 's'
     let pronoun = words.pronoun[randNum3];
     if (pronoun === 'They') {
         return `${words.pronoun[randNum3()]} ${words.adverb[randNum3()]} ${words.activeverb[randNum3()]} ${words.conjunction[randNum3()]} ${words.otherverb[randNum3()]} the ${words.adjectives[randNum3()]} ${words.noun[randNum3()]}.`;
@@ -31,3 +33,5 @@ function returnMessage () {
         return `${words.pronoun[randNum3()]} ${words.adverb[randNum3()]} ${words.activeverb[randNum3()]}s ${words.conjunction[randNum3()]} ${words.otherverb[randNum3()]} the ${words.adjectives[randNum3()]} ${words.noun[randNum3()]}.`;
     }
 }
+
+console.log(returnMessage())
